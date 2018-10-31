119c2e52e1fd466c1536fe6c080a9053896bee6c
Caroline Park <carolpark09@gmail.com>
2018-10-30 21:22:35 -0300

    Remove unnecessary files

--- Ironhack Student Platform_files/constant.js
+++
@@ -1,8 +0,0 @@
-window.domain = 'materials.ironhack.com'
-window.urlpath = ''
-window.debug = false
-window.version = '0.5.1'
-
-window.GOOGLE_API_KEY = ''
-window.GOOGLE_CLIENT_ID = ''
-window.DROPBOX_APP_KEY = ''
